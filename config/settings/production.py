c66367d7664cbc6250bfdfb0aee5d9c7348e9396
Zach Layng <zach@whoknows.com>
2019-10-24 18:08:55 -0700

    (split) chore(whoweb): enable postgres container testing and configure postgres env vars for prod

--- config/settings/production.py
+++ config/settings/production.py
@@ -15,11 +15,15 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["whoknows.com"])
+ALLOWED_HOSTS = env.list(
+    "DJANGO_ALLOWED_HOSTS", default=["whoknows.com", "staging.whoknows.com"]
+)
 
 # DATABASES
 # ------------------------------------------------------------------------------
-DATABASES["default"] = env.db("DATABASE_URL")  # noqa F405
+DATABASES["default"] = "postgresql://postgresql:{}@postgresql/whodb".format(
+    env("postgresql-password")
+)  # noqa F405
 DATABASES["default"]["ATOMIC_REQUESTS"] = True  # noqa F405
 DATABASES["default"]["CONN_MAX_AGE"] = env.int("CONN_MAX_AGE", default=60)  # noqa F405
 

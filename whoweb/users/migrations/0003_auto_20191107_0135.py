8e75bd74d14d4b3b94218bae60aa2bebdbb9a2aa
Zach Layng <zach@whoknows.com>
2019-11-07 03:27:49 -0600

    (split) fix(whoweb):  bypass creating superuser to allow tests to run migrations

--- whoweb/users/migrations/0003_auto_20191107_0135.py
+++ whoweb/users/migrations/0003_auto_20191107_0135.py
@@ -15,10 +15,13 @@ class Migration(migrations.Migration):
         DJANGO_SU_NAME = os.environ.get("DJANGO_SU_NAME")
         DJANGO_SU_EMAIL = os.environ.get("DJANGO_SU_EMAIL")
         DJANGO_SU_PASSWORD = os.environ.get("DJANGO_SU_PASSWORD")
-        superuser = User.objects.create_superuser(
-            username=DJANGO_SU_NAME, email=DJANGO_SU_EMAIL, password=DJANGO_SU_PASSWORD
-        )
+        if DJANGO_SU_EMAIL:
+            superuser = User.objects.create_superuser(
+                username=DJANGO_SU_NAME,
+                email=DJANGO_SU_EMAIL,
+                password=DJANGO_SU_PASSWORD,
+            )
 
-        superuser.save()
+            superuser.save()
 
     operations = [migrations.RunPython(generate_superuser)]

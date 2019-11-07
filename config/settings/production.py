57407feaf96f0d8ff82ce5a5c61b8621a57131f0
Zach Layng <zach@whoknows.com>
2019-11-07 13:44:58 -0600

    (split) fix(whoweb): add ww prefix to staticfiles

--- config/settings/production.py
+++ config/settings/production.py
@@ -19,6 +19,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 ALLOWED_HOSTS = env.list(
     "DJANGO_ALLOWED_HOSTS", default=["whoknows.com", "staging.whoknows.com"]
 )
+USE_X_FORWARDED_HOST = True
 
 # DATABASES
 # ------------------------------------------------------------------------------

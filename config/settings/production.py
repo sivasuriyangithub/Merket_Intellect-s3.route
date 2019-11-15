0ee32524f3c924a3452fb7bb8372c6b4b76a92bc
Zach Layng <zach@whoknows.com>
2019-11-15 13:26:09 -0800

    (split) fix(whoweb): use SSL instead of TLS for google smtp

--- config/settings/production.py
+++ config/settings/production.py
@@ -102,8 +102,8 @@ EMAIL_SUBJECT_PREFIX = env("DJANGO_EMAIL_SUBJECT_PREFIX", default="[WhoWeb] ")
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
-EMAIL_USE_TLS = True
-EMAIL_PORT = 465
+EMAIL_USE_SSL = True
+EMAIL_PORT = 465  # 587 if EMAIL_USE_TLS
 EMAIL_HOST_USER = "team@whoknows.com"
 EMAIL_HOST_PASSWORD = env("SMTP_PASSWORD")
 

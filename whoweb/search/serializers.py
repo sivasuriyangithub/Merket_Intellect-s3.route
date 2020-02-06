836a74f8e92f56c99ac9440db814503a9ddd4f6a
Chris Macomber <chris.macomber@whoknows.com>
2020-02-05 17:33:12 -0800

    (split) Zach suggestions

--- whoweb/search/serializers.py
+++ whoweb/search/serializers.py
@@ -20,7 +20,7 @@ from whoweb.users.models import UserProfile
 class ExportOptionsSerializer(serializers.ModelSerializer):
     class Meta:
         model = ExportOptions
-        fields = ("webhooks", "format", "title", "tags", "metadata")
+        fields = ("webhooks", "format", "title", "metadata")
 
     def to_representation(self, instance):
         return instance.serialize()

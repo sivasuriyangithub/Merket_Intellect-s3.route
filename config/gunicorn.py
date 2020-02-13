36fbab35c1cb8eb62b571a60ebdb24c5d1ac35ed
Zach Layng <zach@whoknows.com>
2020-02-12 20:21:31 -0800

    (split) chore(whoweb): adjust pod resource requests to measured memory use

--- config/gunicorn.py
+++ config/gunicorn.py
@@ -1,5 +1,7 @@
 import multiprocessing
 
-workers = multiprocessing.cpu_count() * 2 + 1
+workers = min(
+    multiprocessing.cpu_count() * 2 + 1, 20
+)  # k8s request should expect ~110mb per process
 timeout = 180
 graceful_timeout = 500

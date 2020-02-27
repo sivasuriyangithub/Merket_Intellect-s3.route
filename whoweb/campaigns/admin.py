a06fa1b9486a6ecfd36a109f614a76daac266ccf
Zach Layng <zach@whoknows.com>
2020-02-26 18:34:51 -0800

    (split) feat(whoweb): add inline admin action to restart campaigns
    PD-4820

--- whoweb/campaigns/admin.py
+++ whoweb/campaigns/admin.py
@@ -1,6 +1,15 @@
 from admin_actions.admin import ActionsModelAdmin
-from django.contrib import admin
+from django.conf import settings
+from django.contrib import admin, messages
+from django.db.models import F
+from django.shortcuts import redirect
+from django.template.defaultfilters import date
+from django.urls import reverse
+from django.utils import timezone
+from inline_actions.admin import InlineActionsMixin, InlineActionsModelAdminMixin
 
+from whoweb.campaigns.events import ENQUEUED_FROM_ADMIN
+from whoweb.coldemail.models import ColdCampaign
 from whoweb.core.admin import EventTabularInline
 from .models import (
     SimpleDripCampaignRunner,
@@ -26,20 +35,77 @@ class DripRecordInline(admin.TabularInline):
         return False
 
 
-class RootCampaignInline(admin.TabularInline):
+class RootCampaignInline(InlineActionsMixin, admin.TabularInline):
     model = BaseCampaignRunner.campaigns.through
     extra = 0
     can_delete = False
+    inline_actions = [
+        "rerun",
+    ]
+    fields = (
+        "coldcampaign",
+        "campaign__status",
+        "campaign__status_changed",
+    )
+    readonly_fields = ("campaign__status", "campaign__status_changed")
 
     verbose_name = "Cold Campaign (root)"
     verbose_name_plural = "Cold Campaigns (roots)"
 
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.annotate(
+            campaign__status=F("coldcampaign__status"),
+            campaign__status_changed=F("coldcampaign__status_changed"),
+        )
+
+    def campaign__status(self, obj):
+        return ColdCampaign.STATUS[obj.campaign__status]
+
+    def campaign__status_changed(self, obj):
+        return date(
+            timezone.localtime(obj.campaign__status_changed), settings.DATETIME_FORMAT
+        )
+
     def has_add_permission(self, request, obj=None):
         return False
 
+    def rerun(self, request, obj, parent_obj=None):
+        cold_campaign = obj.coldcampaign
+        export = cold_campaign.campaign_list.export
+        if export.status != export.STATUS.complete:
+            messages.add_message(
+                request, messages.ERROR, f"Export must be completed to rerun campaign."
+            )
+
+        runner = parent_obj
+        runner.run_id = None
+        runner.save()
+        cold_campaign.status = ColdCampaign.STATUS.created
+        cold_campaign.save()
+        sigs, campaign = runner.publish(apply_tasks=False, with_campaign=cold_campaign)
+        res = sigs.apply_async()
+        messages.add_message(
+            request,
+            messages.SUCCESS,
+            f"{runner} successfully republished starting at {campaign}.",
+        )
+        messages.add_message(request, messages.INFO, f"Tasks run: {sigs}")
+        messages.add_message(request, messages.INFO, f"Result ID: {res}")
+        runner.log_event(
+            evt=ENQUEUED_FROM_ADMIN, signatures=str(sigs), async_result=str(res)
+        )
+        change_url = reverse(
+            "admin:{}_{}_change".format(
+                runner._meta.app_label, runner._meta.model_name,
+            ),
+            args=[runner.pk],
+        )
+        return redirect(change_url)
+
 
 @admin.register(SimpleDripCampaignRunner)
-class SimpleDripCampaignRunnerAdmin(ActionsModelAdmin):
+class SimpleDripCampaignRunnerAdmin(InlineActionsModelAdminMixin, ActionsModelAdmin):
     inlines = [
         SendingRuleInline,
         RootCampaignInline,
@@ -101,7 +167,7 @@ class SimpleDripCampaignRunnerAdmin(ActionsModelAdmin):
 
 
 @admin.register(IntervalCampaignRunner)
-class IntervalCampaignRunnerAdmin(ActionsModelAdmin):
+class IntervalCampaignRunnerAdmin(InlineActionsModelAdminMixin, ActionsModelAdmin):
     inlines = [
         SendingRuleInline,
         RootCampaignInline,
--- whoweb/campaigns/models/base.py
+++ whoweb/campaigns/models/base.py
@@ -148,7 +148,7 @@ class BaseCampaignRunner(
     status_changed = MonitorField("status changed", monitor="status")
     is_removed_changed = MonitorField("deleted at", monitor="is_removed")
     published_at = MonitorField(
-        monitor="status", when=["published"], null=True, default=None, blank=True
+        monitor="status", when=[STATUS.published], null=True, default=None, blank=True
     )
 
     tracking_params = JSONField(default=dict, null=True, blank=True)
@@ -380,11 +380,8 @@ class BaseCampaignRunner(
     def create_campaign_list(self, *args, **kwargs):
         return CampaignList.objects.create(query=self.query, origin=2, seat=self.seat)
 
-    def create_campaign(self, campaign_kwargs=None, *args, **kwargs):
+    def create_campaign(self, **campaign_kwargs):
         first_message_rule = self.sending_rules().first()
-
-        if campaign_kwargs is None:
-            campaign_kwargs = {}
 
         if self.seat:
             campaign_kwargs.setdefault("seat", self.seat)
@@ -412,7 +409,13 @@ class BaseCampaignRunner(
     #                 add_to_set__messages=InboxMessage(id=ObjectId(), campaign_message=message_id, source=source))
 
     def publish(
-        self, apply_tasks=True, on_complete=None, task_context=None, *args, **kwargs
+        self,
+        apply_tasks=True,
+        on_complete=None,
+        task_context=None,
+        with_campaign=None,
+        *args,
+        **kwargs,
     ):
         """
         :rtype: (celery.canvas.Signature | celery.result.AsyncResult | None,  Campaign | None)
@@ -420,13 +423,17 @@ class BaseCampaignRunner(
         from whoweb.campaigns.tasks import set_published
 
         self.log_event(PUBLISH_CAMPAIGN, task=task_context)
-        campaign = self.create_campaign()
+        if with_campaign:
+            campaign = with_campaign
+        else:
+            campaign = self.create_campaign()
         if not campaign:
             return None, None
         if publish_sigs := campaign.publish(apply_tasks=False, on_complete=on_complete):
             if self.run_id is None:
                 self.run_id = uuid.uuid4()
                 self.status = self.STATUS.pending
+                self.save()
 
             publish_sigs |= set_published.si(pk=self.pk, run_id=self.run_id)
             if drip_sigs := self.drip_tasks(
@@ -434,13 +441,12 @@ class BaseCampaignRunner(
             ):
                 publish_sigs |= drip_sigs
 
-            self.log_event(
-                CAMPAIGN_SIGNATURES,
-                task=task_context,
-                data={"sigs": repr(publish_sigs)},
-            )
-            self.save()
             if apply_tasks:
+                self.log_event(
+                    CAMPAIGN_SIGNATURES,
+                    task=task_context,
+                    data={"sigs": repr(publish_sigs)},
+                )
                 return publish_sigs.apply_async(), campaign
             else:
                 return publish_sigs, campaign
